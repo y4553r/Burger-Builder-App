2751d8a51366229003886a7462e29abe8cb04b8d
y4553r <abougueniyasser@outlook.com>
2019-04-04 10:06:36 +0100

    Add checkValidity to utilities

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
 import axios from '../../../axios-orders';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
   constructor(props) {
@@ -101,28 +101,10 @@ class ContactData extends Component {
     formIsValid: false
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.exactLength) {
-      isValid = value.length === rules.exactLength && isValid;
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (event, el) => {
-    const updateFormElement = updateObject(this.state.orderForm, {
+    const updateFormElement = updateObject(this.state.orderForm[el], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[el].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[el].validation),
       shouldValidate: true
     });
     const updatedOrderForm = updateObject(this.state.orderForm, {
@@ -133,7 +115,7 @@ class ContactData extends Component {
     for (let element in updatedOrderForm) {
       formIsValid = this.state.orderForm[element].valid && formIsValid;
     }
-    this.setState({ orderForme: updatedOrderForm, formIsValid: formIsValid });
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   }
 
   orderConfirmedHandler(event) {
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,21 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...updatedProperties
   };
 };
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+
+  if (!rules) {
+    return true;
+  }
+
+  if (rules.required) {
+    isValid = value.trim() !== '' && isValid;
+  }
+
+  if (rules.exactLength) {
+    isValid = value.length === rules.exactLength && isValid;
+  }
+
+  return isValid;
+};
